c7359f9453c56a8c256db4c548355d293d377bfd
nilesh-r <nileshsahu8674@gmail.com>
2025-03-06 10:46:16 +0530

    update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/upload", upload.single("file"), (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 
